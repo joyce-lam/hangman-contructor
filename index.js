36d0622a631e88c177edf7b859b3cc4f1d0a7322
Joyce <joycelamsaungan@gmail.com>
2018-02-19 17:00:03 -0800

    adding functions to validate input and modifying exisiting functions

--- index.js
+++ index.js
@@ -1,62 +1,65 @@
 var inquirer = require("inquirer");
-
 var Word = require("./word");
 var Letter = require("./letter");
 
 
 var wordList = ["afghanistan", "albania", "algeria", "andorra", "angola", "argentina", "armenia", "aruba", "australia", "austria", "azerbaijan", "bahamas", "bahrain", "bangladesh", "barbados", "belarus", "belgium", "belize", "benin", "bhutan", "bolivia", "botswana", "brazil", "brunei", "bulgaria", "burma", "burundi", "cambodia", "cameroon", "canada", "chad", "chile", "china", "colombia", "comoros", "croatia", "cuba", "curacao", "cyprus", "czechia", "denmark", "djibouti", "dominica", "ecuador", "egypt", "eritrea", "estonia", "ethiopia", "fiji", "finland", "france", "gabon", "gambia", "georgia", "germany", "ghana", "greece", "grenada", "guatemala", "guinea", "guyana", "haiti", "honduras", "hungary", "iceland", "india", "indonesia", "iran", "iraq", "ireland", "israel", "italy", "jamaica", "japan", "jordan", "kazakhstan", "kenya", "kiribati", "korea", "kosovo", "kuwait", "kyrgyzstan", "laos", "latvia", "lebanon", "lesotho", "liberia", "libya", "liechtenstein", "lithuania", "luxembourg", "macau", "macedonia", "madagascar", "malawi", "malaysia", "maldives", "mali", "malta", "mauritania", "mauritius", "mexico", "micronesia", "moldova", "monaco", "mongolia", "motenegro", "morocco", "mozambique", "namibia", "nauru", "nepal", "netherlands", "nicaragua", "niger", "nigeria", "norway", "oman", "pakistan", "palau", "panama", "paraguay", "peru", "philippines", "poland", "portugal", "qatar", "romania", "russia", "rwanda", "samoa", "senegal", "serbia", "seychelles", "singapore", "slovakia", "slovenia", "somalia", "spain", "sudan", "suriname", "swaziland", "sweden", "switzerland", "syria", "taiwan", "tajikistan", "tanzania", "thailand", "togo", "tonga", "tunisia", "turkey", "turmenistan", "tuvalu", "uganda", "ukraine", "uruguay", "uzbekistan", "vanuatu", "venezuela", "vietnam", "yemen", "zambia", "zimbabwe"];
 
-//set variables
-//var wins = 0;
-//var losses = 0;
-//var remainedGuesses = 10; 
-//var lettersGuessed = [];
+
 var guessWord;
 var guessWordArr = [];
 var noOfGuesses = 5;
 var lettersGuessed = [];
-var userInputArr = [];
+
 
 // declare a function to find a word within the array
 function randomWord () { 
 	guessWord = wordList[Math.floor(Math.random()*wordList.length)];
 	console.log(guessWord);
+	return guessWord;
 };
 
 randomWord();
 
 
-	
 for (var i = 0; i < guessWord.length; i++) {
 	guessWordArr.push(new Letter(guessWord.charAt(i)));
 };
 
-
-var wordA = new Word(guessWordArr);
-
-console.log("Guess this word: ")
-wordA.display();
-console.log("Guesses you have: " + noOfGuesses);
+var newWord = new Word(guessWordArr);
+console.log("Guess this word: ");
 
 
 var askQuestion = function() {
-	if (noOfGuesses >= 0) {
+	if (noOfGuesses > 0) {
+		newWord.display();
+		console.log("Guesses you have: " + noOfGuesses);
 		inquirer.prompt([
 			{
 				name: "question",
-				message: "What letter is in your mind?"
-
+				message: "What letter is in your mind?",
+				validate: validateString
 			}
 		]).then(function(answer) {
-			lettersGuessed.push(answer.question);
-			console.log("You have guessed: " + lettersGuessed.join(" "));
+			var guessedLetter = answer.question;
 
-			userInputArr.push(answer.question);
-			checkRepeat();
+			var alreadyGuessed = false;
+			for (var i = 0; i < lettersGuessed.length; i++) {
+				if (guessedLetter === lettersGuessed[i]) {
+					alreadyGuessed = true;
+					break;
+				}
+			}
 
+			if (alreadyGuessed) {
+				console.log("You have already guessed this letter. Try again.");
+			} else {
+				noOfGuesses--;
+				lettersGuessed.push(guessedLetter);
 
-			wordA.checkIfCorrect(answer.question);
-			wordA.display();
+				newWord.checkIfCorrect(answer.question);
+			}
+
 
 			askQuestion();
 
@@ -69,21 +72,11 @@ var askQuestion = function() {
 askQuestion();
 
 
-function checkRepeat() {
-	for (var i = 0; i < lettersGuessed.length; i++) {
-		for (var j = 1; j <userInputArr.length; j++) {
-			if (userInputArr[j] = lettersGuessed[i]) {
-				console.log("You have guessed this letter already! Try another one.");
-				console.log(noOfGuesses);
-			} else {
-				countGuesses();
-			}
-		};
-	};
-}
-
-
-function countGuesses() {
-	noOfGuesses--;
-	console.log("Guesses left: " + noOfGuesses);
+function validateString(char) {
+	if (char.match(/[A-Z]/gi)) {
+		return true;
+	} else {
+		console.log("Please enter a valid letter from a - z");
+	}
 };
+
